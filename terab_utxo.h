e008bb98605697b0c9fd258a5e350c0d77fdaa5b
Joannes Vermorel <joannes.vermorel@lokad.com>
2018-01-30 10:56:47 +0100

    Opaque connection handle introduced

--- terab_utxo.h
+++ terab_utxo.h
@@ -72,8 +72,25 @@ struct txo
 	tx_payload payload;
 };
 
+/* Get a connection handle intended for all Terab-related operations.
+
+   connection_string: details to connect to the Terab instance.
+   conn: returned as an opaque connection handle.
+
+   For various purposes, e.g. testing or failover, there might be
+   multiple Terab instances available. The connection handle offers
+   the possibility to interact with multiple instances without
+   implicitly enforcing a "shared" configuration at the process
+   level.
+*/
+int32_t terab_connect(
+	char* connection_string,
+	void* conn
+);
+
 /* Get the metadata associated to a block.
 
+   conn: opaque connection handle.
    block: identifies the targeted block.
    info: contains the response, if any.
 
@@ -84,12 +101,14 @@ struct txo
    This method is PURE.
 */
 int32_t terab_uxto_get_blockinfo(
+	void* conn,
 	int32_t block,
 	block_info* info
 );
 
 /* Get the metadata associated to outpoints.
   
+   conn: opaque connection handle.
    block: identifies the block of reference.
    output_length: indicates how many outputs are to be queried.
    outputs: identifies the outpoints to be queried.
@@ -98,6 +117,7 @@ int32_t terab_uxto_get_blockinfo(
    The method is PURE.
 */
 int32_t terab_utxo_get(
+	void* conn,
 	int32_t block,
 	int32_t outpoint_length,
 	tx_outpoint* outpoints,
@@ -106,6 +126,7 @@ int32_t terab_utxo_get(
 
 /* Starts the write sequence for a new block.
    
+   conn: opaque connection handle.
    parent: identifies the parent of the new block.
    blockid: the 32-byte hash of the new block.
    block: returned as the compact identifier of the new block.
@@ -117,6 +138,7 @@ int32_t terab_utxo_get(
    This method is IDEMPOTENT.
 */
 int32_t terab_utxo_open_block(
+	void* conn,
 	int32_t parent,
 	uint8_t* blockid,
 	int32_t* block
@@ -124,6 +146,7 @@ int32_t terab_utxo_open_block(
 
 /* Write new outputs and their payloads to a new block.
   
+  conn: opaque connection handle.
   block: identifies the block being written to.
   txo_length: specifies the number of outpoints to be written.
   txos: specifies what should be written to the block.
@@ -135,6 +158,7 @@ int32_t terab_utxo_open_block(
   This method is IDEMPOTENT.
 */
 int32_t terab_utxo_write_txs(
+	void* conn,
 	int32_t block,
 	int32_t txo_length,
 	txo* txos
@@ -142,6 +166,7 @@ int32_t terab_utxo_write_txs(
 
 /* Closes the write sequence for a new block.
    
+   conn: opaque connection handle.
    block: identifies the block written to.
 
    In UTXO configuration, any call to a block a block that 
@@ -151,5 +176,6 @@ int32_t terab_utxo_write_txs(
    This method is IDEMPOTENT.
 */
 int32_t terab_utxo_commit_block(
+	void* conn,
 	int32_t block
 );
